2f6560855bb5d74f0acb0397f7327be6b34e2201
zengxing358 <478963019@qq.com>
2021-11-27 15:46:09 +0800

    first commit

---
+++ week1/00.poetry/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("静夜思")
+	fmt.Println("床前明月光，")
+	fmt.Println("疑是地上霜。")
+	fmt.Println("举头望明月，")
+	fmt.Println("低头思故乡。")
+}
---
+++ week1/00.star/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("                         *                        ")
+	fmt.Println("                        ***                       ")
+	fmt.Println("                       *****                      ")
+	fmt.Println("                      *******                     ")
+	fmt.Println("                     *********                    ")
+	fmt.Println("                    ***********                   ")
+	fmt.Println("  *********************************************   ")
+	fmt.Println("     ***************************************      ")
+	fmt.Println("        *********************************         ")
+	fmt.Println("           ***************************            ")
+	fmt.Println("              *********************               ")
+	fmt.Println("               *******************                ")
+	fmt.Println("              *********************               ")
+	fmt.Println("             ***********************              ")
+	fmt.Println("            ***********   ***********             ")
+	fmt.Println("           *********         *********            ")
+	fmt.Println("          *******               *******           ")
+	fmt.Println("         *****                     *****          ")
+	fmt.Println("        ***                           ***         ")
+	fmt.Println("       *                                 *        ")
+}
